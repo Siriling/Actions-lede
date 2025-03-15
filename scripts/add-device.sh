a04a44fc17d8d5b577851e9ed6122e1ffccd88a0
siriling <siriling@qq.com>
2025-03-15 12:24:42 +0800

    更新添加设备脚本

--- scripts/add-device.sh
+++ scripts/add-device.sh
@@ -24,11 +24,11 @@ tar -xvf rtl8367b.tar.gz
 # wget https://github.com/xiaomeng9597/files/releases/download/files/rtl8367b-openwrt.tar.gz
 # tar -xvf rtl8367b-openwrt.tar.gz
 
-# rm -f target/linux/rockchip/rk35xx/base-files/lib/board/init.sh
-# cp -f $GITHUB_WORKSPACE/patchs/nsy/init.sh target/linux/rockchip/rk35xx/base-files/lib/board/init.sh
+# rm -f target/linux/rockchip/armv8/base-files/lib/board/init.sh
+# cp -f $GITHUB_WORKSPACE/patchs/nsy/init.sh target/linux/rockchip/armv8/base-files/lib/board/init.sh
 
-rm -f target/linux/rockchip/rk35xx/base-files/etc/board.d/02_network
-cp -f $GITHUB_WORKSPACE/patchs/nsy/02_network target/linux/rockchip/rk35xx/base-files/etc/board.d/02_network
+rm -f target/linux/rockchip/armv8/base-files/etc/board.d/02_network
+cp -f $GITHUB_WORKSPACE/patchs/nsy/02_network target/linux/rockchip/armv8/base-files/etc/board.d/02_network
 
 # 增加nsy_g68-plus
 echo -e "\\ndefine Device/nsy_g68-plus
@@ -39,7 +39,7 @@ echo -e "\\ndefine Device/nsy_g68-plus
   SUPPORTED_DEVICES += nsy,g68-plus
   DEVICE_PACKAGES := kmod-nvme kmod-scsi-core kmod-thermal kmod-switch-rtl8306 kmod-switch-rtl8366-smi kmod-switch-rtl8366rb kmod-switch-rtl8366s kmod-hwmon-pwmfan kmod-leds-pwm kmod-r8125 kmod-r8168 kmod-switch-rtl8367b swconfig kmod-swconfig
 endef
-TARGET_DEVICES += nsy_g68-plus" >> target/linux/rockchip/image/rk35xx.mk
+TARGET_DEVICES += nsy_g68-plus" >> target/linux/rockchip/image/armv8.mk
 
 # 增加nsy_g16-plus
 echo -e "\\ndefine Device/nsy_g16-plus
@@ -50,7 +50,7 @@ echo -e "\\ndefine Device/nsy_g16-plus
   SUPPORTED_DEVICES += nsy,g16-plus
   DEVICE_PACKAGES := kmod-nvme kmod-scsi-core kmod-thermal kmod-switch-rtl8306 kmod-switch-rtl8366-smi kmod-switch-rtl8366rb kmod-switch-rtl8366s kmod-hwmon-pwmfan kmod-leds-pwm kmod-r8125 kmod-r8168 kmod-switch-rtl8367b swconfig kmod-swconfig
 endef
-TARGET_DEVICES += nsy_g16-plus" >> target/linux/rockchip/image/rk35xx.mk
+TARGET_DEVICES += nsy_g16-plus" >> target/linux/rockchip/image/armv8.mk
 
 # 增加bdy_g18-pro
 echo -e "\\ndefine Device/bdy_g18-pro
@@ -61,9 +61,9 @@ echo -e "\\ndefine Device/bdy_g18-pro
   SUPPORTED_DEVICES += bdy,g18-pro
   DEVICE_PACKAGES := kmod-nvme kmod-scsi-core kmod-thermal kmod-switch-rtl8306 kmod-switch-rtl8366-smi kmod-switch-rtl8366rb kmod-switch-rtl8366s kmod-hwmon-pwmfan kmod-leds-pwm kmod-r8125 kmod-r8168 kmod-switch-rtl8367b swconfig kmod-swconfig
 endef
-TARGET_DEVICES += bdy_g18-pro" >> target/linux/rockchip/image/rk35xx.mk
+TARGET_DEVICES += bdy_g18-pro" >> target/linux/rockchip/image/armv8.mk
 
 # 添加dts
-cp -f $GITHUB_WORKSPACE/patchs/nsy/rk3568-nsy-g68-plus.dts target/linux/rockchip/dts/rk3568/rk3568-nsy-g68-plus.dts
-cp -f $GITHUB_WORKSPACE/patchs/nsy/rk3568-nsy-g16-plus.dts target/linux/rockchip/dts/rk3568/rk3568-nsy-g16-plus.dts
-cp -f $GITHUB_WORKSPACE/patchs/nsy/rk3568-bdy-g18-pro.dts target/linux/rockchip/dts/rk3568/rk3568-bdy-g18-pro.dts
+cp -f $GITHUB_WORKSPACE/patchs/nsy/rk3568-nsy-g68-plus.dts target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/rk3568-nsy-g68-plus.dts
+cp -f $GITHUB_WORKSPACE/patchs/nsy/rk3568-nsy-g16-plus.dts target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/rk3568-nsy-g16-plus.dts
+cp -f $GITHUB_WORKSPACE/patchs/nsy/rk3568-bdy-g18-pro.dts target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/rk3568-bdy-g18-pro.dts
